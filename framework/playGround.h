c4f2dfc3d45f48d2b7a23cc082e37eb42218356b
toto9811 <toto9811@naver.com>
2021-06-18 11:49:08 +0900

    대충 점프랑 픽셀충돌

--- framework/playGround.h
+++ framework/playGround.h
@@ -9,7 +9,16 @@ class playGround : public gameNode
 private:
 	char* _str;
 	int _test;
-
+	bool _direction;
+	image* _player;
+	int _state;
+	bool _isMove;
+	float _playerX, _playerY;
+	float _playerZ;
+	float _gravity;
+	float _jumpPower;
+	bool _isJump;
+	int _probeY;
 public:
 	playGround();
 	~playGround();
